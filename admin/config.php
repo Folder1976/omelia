8da92f6b99ab6414b7552bf36ca4a742b86bf879
Folder <folder.list@gmail.com>
2018-07-16 17:00:18 +0300

    start

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://omeliya.like-it.solutions/admin/');
+define('HTTP_CATALOG', 'http://omeliya.like-it.solutions/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://omeliya.like-it.solutions/admin/');
+define('HTTPS_CATALOG', 'http://omeliya.like-it.solutions/');
+
+// DIR
+define('DIR_APPLICATION', '/home/vs278668/like-it.solutions/omeliya/admin/');
+define('DIR_SYSTEM', '/home/vs278668/like-it.solutions/omeliya/system/');
+define('DIR_IMAGE', '/home/vs278668/like-it.solutions/omeliya/image/');
+define('DIR_LANGUAGE', '/home/vs278668/like-it.solutions/omeliya/admin/language/');
+define('DIR_TEMPLATE', '/home/vs278668/like-it.solutions/omeliya/admin/view/template/');
+define('DIR_CONFIG', '/home/vs278668/like-it.solutions/omeliya/system/config/');
+define('DIR_CACHE', '/home/vs278668/like-it.solutions/omeliya/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/vs278668/like-it.solutions/omeliya/system/storage/download/');
+define('DIR_LOGS', '/home/vs278668/like-it.solutions/omeliya/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/vs278668/like-it.solutions/omeliya/system/storage/modification/');
+define('DIR_UPLOAD', '/home/vs278668/like-it.solutions/omeliya/system/storage/upload/');
+define('DIR_CATALOG', '/home/vs278668/like-it.solutions/omeliya/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'vs278668.mysql.tools');
+define('DB_USERNAME', 'vs278668_omel');
+define('DB_PASSWORD', 'px6hve3h');
+define('DB_DATABASE', 'vs278668_omel');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'om_');
